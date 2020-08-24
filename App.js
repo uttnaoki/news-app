5594049ae66c1f4f28155fb7546291d3630a6c02
Naoki Kinoshita <uttnaoki@gmail.com>
2020-08-24 20:42:29 +0900

    sec4: レイアウト区画整理

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>xxxxx</Text>
-      <StatusBar style="auto" />
+      <View style={styles.itemContainer}>
+        <View style={styles.leftContainer} />
+        <View style={styles.rightContainer} />
+      </View>
     </View>
   );
 }
@@ -15,8 +16,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  itemContainer: {
+    height: 100,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    flexDirection: 'row',
+  },
+  leftContainer: {
+    backgroundColor: 'red',
+    width: 100,
+  },
+  rightContainer: {
+    backgroundColor: 'blue',
+    flex: 1,
   },
 });
