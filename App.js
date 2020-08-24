71bec11188bc4adcb97350f941d44ea08cac34b8
Naoki Kinoshita <uttnaoki@gmail.com>
2020-08-24 22:32:29 +0900

    sec6: useState

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View, FlatList, SafeAreaView } from 'react-native';
 import ListItem from './components/ListItem';
-import articles from './dummies/articles';
+import dummyArticles from './dummies/articles';
 
 export default function App() {
+  const [articles, setArticles] = useState(dummyArticles);
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
