e83784c24d9f6a5e55f5d30e7f068315c359f802
Naoki Kinoshita <uttnaoki@gmail.com>
2020-08-24 21:38:31 +0900

    sec5: コンポーネント切出し

---
+++ components/ListItem.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+const ListItem = () => {
+  return (
+    <View style={styles.itemContainer}>
+      <View style={styles.leftContainer}>
+        <Image
+          style={{ width: 100, height: 100 }}
+          source={{
+            uri: 'https://picsum.photos/id/10/200/200',
+          }}
+        />
+      </View>
+      <View style={styles.rightContainer}>
+        <Text numberOfLines={3} style={styles.text}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </Text>
+        <Text style={styles.subText}>ReactNews</Text>
+      </View>
+    </View>
+  );
+};
+
+export default ListItem;
+
+const styles = StyleSheet.create({
+  itemContainer: {
+    height: 100,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    flexDirection: 'row',
+  },
+  leftContainer: {
+    width: 100,
+  },
+  rightContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    padding: 10,
+    justifyContent: 'space-between',
+  },
+  text: {
+    fontSize: 16,
+  },
+  subText: {
+    fontSize: 12,
+    color: 'gray',
+  },
+});
